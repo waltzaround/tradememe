f1f21a537c37a9bb2efccb530579b234898becb6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-05-12 23:45:07 +1200

    add bodyParser
    update package loc

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -3,10 +3,12 @@ import { connectToDb } from './Connections';
 import cors from 'cors';
 import config from './config';
 import registerRoutes from './routes';
+import bodyParser  from 'body-parser'
 
 function startServer() {
     const app = express();
     app.use(cors())
+    app.use(bodyParser.json())
     registerRoutes(app);
     app.listen(config.port, function() {
         connectToDb().then(() => {
