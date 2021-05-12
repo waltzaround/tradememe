89f036a22404554bd37428b22bcc1d6b97c436d7
Walter Lim <waltissomewhere@gmail.com>
2021-05-12 23:58:31 +1200

    push changes

--- client/app.js
+++ client/app.js
@@ -1,37 +1,49 @@
-'use strict'
+"use strict";
 
-const API_URL = location.host === 'tradememe.club' ? 'https://api.tradememe.club' : 'http://localhost:8080'
+const API_URL =
+  location.host === "tradememe.club"
+    ? "https://api.tradememe.club"
+    : "http://localhost:8080";
 let auctions = [];
 
 function loadApp() {
-    getListings().then((data) => {
-        auctions = data;
-        const listingsHtml = data.map(auction => generateListing(auction._id, auction.title, 1, auction.image)).join('')
-        document.getElementById('listings').innerHTML = listingsHtml;
-    })
+  getListings().then((data) => {
+    auctions = data;
+    const listingsHtml = data
+      .map((auction) =>
+        generateListing(auction._id, auction.title, 1, auction.image)
+      )
+      .join("");
+    document.getElementById("listings").innerHTML = listingsHtml;
+  });
 }
 
 function handleSearchChange(search) {
-    const matchedAuctions = auctions.filter(auction => auction.title.includes(search));
-    const filteredListingsHtml = matchedAuctions.map(auction => generateListing(auction._id, auction.title, 1, auction.image)).join('')
-    document.getElementById('listings').innerHTML = filteredListingsHtml;
+  const matchedAuctions = auctions.filter((auction) =>
+    auction.title.includes(search)
+  );
+  const filteredListingsHtml = matchedAuctions
+    .map((auction) =>
+      generateListing(auction._id, auction.title, 1, auction.image)
+    )
+    .join("");
+  document.getElementById("listings").innerHTML = filteredListingsHtml;
 }
 
-
 async function getListings() {
-    const res = await fetch(`${API_URL}/auctions`);
-    const data = await res.json();
-    return data.auctions;
+  const res = await fetch(`${API_URL}/auctions`);
+  const data = await res.json();
+  return data.auctions;
 }
 
 function incrementBid(elementId) {
-    const currentBid = document.getElementById(elementId).innerText;
-    document.getElementById(elementId).innerText = parseInt(currentBid) + 1;
-    new Audio('./monies.mp3').play();
+  const currentBid = document.getElementById(elementId).innerText;
+  document.getElementById(elementId).innerText = parseInt(currentBid) + 1;
+  new Audio("./monies.mp3").play();
 }
 
 function generateListing(id, title, currentBid, image) {
-    return `
+  return `
         <div class="listing">
             <img class="photo" src="${image}" />
             <div class="details-container">
@@ -46,4 +58,21 @@ function generateListing(id, title, currentBid, image) {
             </div>
         </div>
     `;
+}
+
+async function submitMeme() {
+  var name = document.getElementById("meme-name").value;
+  var imagememe = document.getElementById("meme-image").value;
+  //   var price = document.getElementById("meme-price").value;
+
+  const res = await fetch(`${API_URL}/auctions`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appication/json",
+    },
+    body: JSON.stringify({ title: name, image: imagememe }),
+  });
+
+  const data = await res.json();
 }
